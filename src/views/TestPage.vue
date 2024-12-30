7a96b16dff3fbd9cac3daf90cc9822f55617b105
chr15z <chrisztlr@gmail.com>
2024-12-30 16:18:22 +0100

    added components for search bar

--- src/views/TestPage.vue
+++ src/views/TestPage.vue
@@ -1,5 +1,15 @@
 <script setup lang="ts">
 import { ref, watch } from 'vue';
+import {
+  Command,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+  CommandList,
+  CommandSeparator,
+} from '@/components/ui/command'
+
 
 const inputValue = ref('');
 const vaccines = ref<{ name: string; manufacturer: string }[]>([]);
@@ -144,6 +154,40 @@ watch(inputValue, () => {
       </ul>
     </div>
   </div>
+
+
+
+
+  <Command>
+    <CommandInput placeholder="Type a command or search..." />
+    <CommandList>
+      <CommandEmpty>No results found.</CommandEmpty>
+      <CommandGroup heading="Suggestions">
+        <CommandItem value="calendar">
+          Calendar
+        </CommandItem>
+        <CommandItem value="search-emoji">
+          Search Emoji
+        </CommandItem>
+        <CommandItem value="calculator">
+          Calculator
+        </CommandItem>
+      </CommandGroup>
+      <CommandSeparator />
+      <CommandGroup heading="Settings">
+        <CommandItem value="profile">
+          Profile
+        </CommandItem>
+        <CommandItem value="billing">
+          Billing
+        </CommandItem>
+        <CommandItem value="settings">
+          Settings
+        </CommandItem>
+      </CommandGroup>
+    </CommandList>
+  </Command>
+
 </template>
 
 <style scoped>
