6fcfc173f297003bd90040f7e5371fc2846cdcc4
12024736 <e12024736@student.tuwien.ac.at>
2024-12-30 15:26:02 +0100

    NodeDetail view

---
+++ src/components/NodeDetail.vue
@@ -0,0 +1,52 @@
+<script setup>
+import {Button} from '@/components/ui/button/index.js';
+
+export default {
+  name: 'NodeDetail',
+  data() {
+    return {
+      responseData: null,
+    };
+  },
+  methods: {
+    async fetchNodeDetails() {
+      const id = '6766d85a6d8ba24d19d486a2';
+      const journey_id = 1;
+      const node_id = 1;
+      try {
+        const baseUrl = 'https://n8n.tonii.at/webhook/node';
+        const response = await fetch(`${baseUrl}?id=${id}&journey_id=${journey_id}&node_id=${node_id}`);
+        if (!response.ok) {
+          throw new Error(`HTTP error fetching n8n: ${response.status}`);
+        }
+        this.responseData = await response.json();
+      } catch (error) {
+        console.error('Error fetching node:', error);
+      }
+    },
+  },
+};
+</script>
+
+<template>
+  <div>
+    <button @click="fetchNodeDetails">
+      Node Details
+    </button>
+    <Button @click="fetchNodeDetails">
+      Node Details
+    </Button>
+    <div v-if="responseData">
+      <h2>Node name:</h2>
+      <ul>
+        <li v-for="(value, key) in responseData[0]" :key="key">
+          <strong>{{ key }}:</strong> {{ value }}
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+
+<style scoped>
+</style>
--- src/router/index.ts
+++ src/router/index.ts
@@ -2,10 +2,12 @@ import { createRouter, createWebHistory } from 'vue-router'
 import AuthPage from '@/views/AuthPage.vue'
 import MainJourneyPage from '@/views/MainJourneyPage.vue'
 import TestPage from "@/views/TestPage.vue";
+import NodeDetail from '@/components/NodeDetail.vue'
 
 const routes = [
   { path: '/', redirect: '/login' }, // default to login page
   { path: '/login', name: 'Login', component: AuthPage },
+  { path: '/node', name: 'Node', component: NodeDetail },
   { path: '/main', name: 'Main', component: MainJourneyPage },
   { path: '/test', name: 'Test', component: TestPage }
 ]
