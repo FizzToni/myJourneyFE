318e20392d2e294bbf1637f8b72416e279f13b41
12024736 <e12024736@student.tuwien.ac.at>
2025-01-08 15:00:02 +0100

    addJourney route

--- src/router/index.ts
+++ src/router/index.ts
@@ -4,12 +4,14 @@ import MainJourneyPage from '@/views/MainJourneyPage.vue'
 import TestPage from "@/views/TestPage.vue";
 import NodeDetail from '@/components/NodeDetail.vue'
 import AddNode from "@/components/AddNode.vue";
+import AddJourney from "@/components/AddJourney.vue";
 
 const routes = [
   { path: '/', redirect: '/login' }, // default to login page
   { path: '/login', name: 'Login', component: AuthPage },
   { path: '/node', name: 'Node', component: NodeDetail },
   { path: '/addNode', name: 'AddNode', component: AddNode },
+  { path: '/addJourney', name: 'AddJourney', component: AddJourney },
   { path: '/main', name: 'Main', component: MainJourneyPage },
   { path: '/test', name: 'Test', component: TestPage }
 ]
