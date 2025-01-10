8c044741198e05fd5772064c45ab10f2789d9465
chr15z <chrisztlr@gmail.com>
2025-01-10 18:07:47 +0100

    styling

--- src/views/OverviewPage.vue
+++ src/views/OverviewPage.vue
@@ -1,81 +1,99 @@
 <template>
-  <div class="safe-area">
-    <div class="container">
-      <div class="container-content">
-        <div class="row">
-          <div class="info">
-            <div class="flex-between">
-              <h2 class="headline">Vaccination Tracker</h2>
-              <i class="material-icons"
-                 @click="handleVaccineClick()">
-                >
-              </i>
+    <!-- Banner -->
+    <banner title="Overview" status="" :on-refresh="fetchJourneys"/>
+
+    <div class="actual-content">
+      <div class="safe-area">
+      <div class="container">
+        <div class="container-content">
+          <div class="row">
+            <div class="info">
+              <div class="flex-between">
+                <h2 class="headline">Vaccination Tracker</h2>
+                <i class="material-icons"
+                   @click="handleVaccineClick()">
+                  >
+                </i>
+              </div>
+              <p class="description">
+                Keep track of your vaccinations easily and stay informed.
+              </p>
             </div>
-            <p class="description">
-              Keep track of your vaccinations easily and stay informed.
-            </p>
           </div>
-        </div>
-        <div class="row">
-          <div class="info">
-            <h3 class="number" v-if="vaccines">
-              {{ vaccines.length }}
-            </h3>
-            <p class="description">View your complete vaccination history.</p>
-          </div>
-          <div class="info">
-            <p class="notification bg-red">12 new</p>
-            <p class="description">
-              Discover more insights and recommendations.
-            </p>
+          <div class="row">
+            <div class="info">
+              <h3 class="number" v-if="vaccines">
+                {{ vaccines.length }}
+              </h3>
+              <p class="description">View your complete vaccination history.</p>
+            </div>
+            <div class="info">
+              <p class="notification bg-red">12 new</p>
+              <p class="description">
+                Discover more insights and recommendations.
+              </p>
+            </div>
           </div>
         </div>
       </div>
     </div>
-  </div>
 
-  <div>
-    <div class="list-view">
-      <div
-        v-for="(item, index) in nodes"
-        :key="index"
-        class="container padded flex-basis-30"
-        @click="item.onClick"
-      >
-        <div>
-          <h3 class="number">{{ item.number }}</h3>
-          <p class="description">{{ item.text }}</p>
+        <div class="list-view">
+          <div
+            v-for="(item, index) in nodes"
+            :key="index"
+            class="container padded flex-basis-30"
+            @click="item.onClick"
+          >
+            <div>
+              <h3 class="number">{{ item.number }}</h3>
+              <p class="description">{{ item.text }}</p>
+            </div>
+          </div>
         </div>
-      </div>
-    </div>
-  </div>
 
-  <div>
-    <div class="list-view">
-      <div
-        v-for="(journey, index) in journeys"
-        :key="index"
-        class="container journey-card"
-        @click="handleJourneyClick(journey.journey_id)"
-      >
-        <div>
-          <h3 class="number">{{ journey.nodes.length }}</h3>
-          <p class="description">{{ journey.title }}</p>
-          <p class="description">{{ journey.status }}</p>
+      <div>
+      <div class="list-view">
+        <div
+          v-for="(journey, index) in journeys"
+          :key="index"
+          class="container journey-card"
+          @click="handleJourneyClick(journey.journey_id)"
+        >
+          <div>
+            <h3 class="number">{{ journey.nodes.length }}</h3>
+            <p class="description">{{ journey.title }}</p>
+            <p class="description">{{ journey.status }}</p>
+          </div>
         </div>
       </div>
     </div>
-  </div>
+
+    </div>
+
+    <!-- Navbar -->
+    <navbar/>
 </template>
 
 <script>
+import Banner from "@/components/banner/banner.vue";
+import Navbar from "@/components/navbar/Navbar.vue";
+import Addbutton from "@/components/addbutton/Addbutton.vue";
+import Modal from "@/components/Modal/Modal.vue";
+
 let user_id = ''
 export default {
+  components: {
+    Banner,
+    Navbar,
+    Addbutton,
+    Modal,
+  },
   data() {
     return {
       nodes: [],
       journeys: [],
-      vaccines: []
+      vaccines: [],
     }
   },
   methods: {
@@ -85,7 +103,11 @@ export default {
         const response = await fetch(adresse)
         const data = await response.json()
         if (data && data[0] && data[0].journeys) {
-          this.journeys = data[0].journeys
+          for (let i = 0; i < data[0].journeys.length; i++) {
+            if (data[0].journeys[i].status === "active"){
+              this.journeys.push(data[0].journeys[i]);
+            }
+          }
           this.vaccines = data[0].vaccines
         }
 
@@ -128,15 +150,15 @@ export default {
     handleJourneyClick(journeyId) {
       console.log('User ID:' + user_id)
       console.log('Journey ID:', journeyId)
-    }
+    },
   },
   mounted() {
     user_id = '677ba8958eca95927318b059'
     this.fetchJourneys()
   }
 }
-
 </script>
+
 
 <style scoped>
 .list-view {
@@ -233,4 +255,12 @@ export default {
 .notification.bg-red {
   background-color: red;
 }
+
+.actual-content {
+  /*das muss sich dynamisch anpassen (falls mehr als 6 journeys angezeigt werden)*/
+  max-height: calc(100vh - 92px - 123px);
+  overflow-y: auto;
+  box-sizing: border-box;
+}
+
 </style>
